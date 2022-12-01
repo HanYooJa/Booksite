33fd80f71f799c1a9c5a5afdd0b10ecda6c5c4b6
HanYooJa <hyj03250@naver.com>
2022-12-01 20:27:15 +0900

    5

--- components/DropdownLink.js
+++ components/DropdownLink.js
@@ -4,8 +4,8 @@ import React from 'react'
 export default function DropdownLink(props) {
   let { href, children, ...rest } = props
   return (
-    <Link href={href} {...rest} legacyBehavior>
-      {children}
+    <Link href={href}>
+      <a {...rest}>{children}</a>
     </Link>
-  );
+  )
 }
